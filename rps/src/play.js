728f301009c02aa24762ff00fe62ee1e28aac2de
Robert Gravina <rgravina@pivotal.io>
2019-06-11 14:37:20 +0900

    Method Object refactor.

--- rps/src/play.js
+++ rps/src/play.js
@@ -5,20 +5,38 @@ const THROW = {
 }
 const THROWS = [THROW.ROCK, THROW.PAPER, THROW.SCISSORS]
 
-class Request {
-    play(p1, p2, observer) {
-        if (THROWS.includes(p1) === false ||
-            THROWS.includes(p2) === false) {
+function PlayRequest(p1, p2, observer) {
+    this.process = () => {
+        if (anyThrowInvalid()) {
             observer.invalid()
-        } else if (p1 === p2) {
+        } else if (isDraw()) {
             observer.draw()
-        } else if (p1 === THROW.ROCK && p2 === THROW.SCISSORS ||
-            p1 === THROW.PAPER && p2 === THROW.ROCK ||
-            p1 === THROW.SCISSORS && p2 === THROW.PAPER) {
+        } else if (p1Wins()) {
             observer.p1Wins()
         } else {
             observer.p2Wins()
         }
+
+        function p1Wins() {
+            return p1 === THROW.ROCK && p2 === THROW.SCISSORS ||
+                p1 === THROW.PAPER && p2 === THROW.ROCK ||
+                p1 === THROW.SCISSORS && p2 === THROW.PAPER
+        }
+
+        function isDraw() {
+            return p1 === p2
+        }
+
+        function anyThrowInvalid() {
+            return THROWS.includes(p1) === false ||
+                THROWS.includes(p2) === false
+        }
+    }
+}
+
+class Request {
+    play(p1, p2, observer) {
+        new PlayRequest(p1, p2, observer).process()
     }
 }
 
