f4551c377fed6d96624e64144f69b8d2325ced60
Robert Gravina <rgravina@pivotal.io>
2019-06-11 11:36:57 +0900

    Refactor if statement.

--- rps/src/play.js
+++ rps/src/play.js
@@ -10,22 +10,15 @@ class Request {
         if (THROWS.includes(p1) === false ||
             THROWS.includes(p2) === false) {
             observer.invalid()
-            return
-        }
-
-        if (p1 === p2) {
+        } else if (p1 === p2) {
             observer.draw()
-            return
-        }
-
-        if (p1 === THROW.ROCK && p2 === THROW.SCISSORS ||
+        } else if (p1 === THROW.ROCK && p2 === THROW.SCISSORS ||
             p1 === THROW.PAPER && p2 === THROW.ROCK ||
             p1 === THROW.SCISSORS && p2 === THROW.PAPER) {
             observer.p1Wins()
-            return
+        } else {
+            observer.p2Wins()
         }
-
-        observer.p2Wins()
     }
 }
 
